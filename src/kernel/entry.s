ffc96e93e7bb212f02b039c1432be73aa1ec009e
Harry Fellowes <harryfellowes1@gmail.com>
2025-08-01 17:48:20 +0100

    remove useless code

--- src/kernel/entry.s
+++ src/kernel/entry.s
@@ -21,17 +21,8 @@ _start:
 	; Setup a simple stack
 	mov esp, 0x9FF00
 	and esp, 0xFFFFFFF0
-	
-	; Clear the screen
-	mov edi, 0xB8000
-	mov ecx, 80 * 25
-	mov eax, 0x0720
 
-.clear_loop:
-	stosw
-	loop .clear_loop
-
-	call kernel_main ; kernel.c
+	call kernel_main
 
 .halt:
 	hlt
